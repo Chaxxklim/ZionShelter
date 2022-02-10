df766d9381365bd8475cf0e54926138aee14424e
Chaxxklim <87353327+Chaxxklim@users.noreply.github.com>
2022-02-10 11:05:05 +0900

    login -register add

---
+++ src/main/java/www/gnsoft/zionshelter/mapper/CommentMapper.java
@@ -0,0 +1,12 @@
+package www.gnsoft.zionshelter.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import www.gnsoft.zionshelter.vo.BoardVO;
+import www.gnsoft.zionshelter.vo.CommentVO;
+
+import java.util.List;
+
+@Mapper
+public interface CommentMapper {
+    List<CommentVO> selectCommentByBoardIdx(BoardVO boardVO);
+}
